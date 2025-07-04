ec1c80c5bf357fa116650962437e3b3304f42f14
Khaled ElNaggar <khaledfunnew@gmail.com>
2025-07-04 15:10:22 +0300

    Update api.ts
    Testing the global server

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = ' https://1685-154-176-50-83.ngrok-free.app/api';
 
 const api = axios.create({
   baseURL: API_URL,
@@ -55,4 +55,4 @@ export const auth = {
   getToken: () => localStorage.getItem('token'),
 };
 
-export default api;
+export default ;
